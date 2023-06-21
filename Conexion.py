094099b56984e20bac8909ed2fa8652089229f7b
Aronmercado <131614169+Aronmercado@users.noreply.github.com>
2023-06-20 21:50:25 -0300

    Add files via upload

--- Conexion.py
+++ Conexion.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 #conexion = mysql.connector.connect(
-#    host = "localhost",
+#    host = "BigBread",
 #    port = 3306,
 #    user = "root",
-#    password = "PcGc$121315!",
+#    password = "Root123",
 #)
 
 #cursor = conexion.cursor()
@@ -14,10 +14,10 @@ import mysql.connector
 #    print(bd)
 
 conexion = mysql.connector.connect(
-    host = 'localhost',
+    host = 'BigBread',
     port = 3306,
     user = 'root',
-    password = 'PcGc$121315!',
+    password = 'Root123',
     db = 'db_big_bread'
     )
 
@@ -48,7 +48,58 @@ class Conectar():
         except mysql.connector.Error as descripcionDelError:
             print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
 
+    def Insertar_Insumos(self,idinsumo,nombre,stock,precio,):
+        conection = conexion.Conectar()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "INSERT INTO insumos values(%s,%s,%s,%s)"
+            data = (idinsumo, nombre, stock, precio,)
+            cursor.execute(sentenciaSQL,data)
+            cursor.conexion.commit()
+            cursor.conexion.close()
+
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+    def Insertar_Recetas(self,idreceta,nombre,idproducto,cant_producto,):
+        conection = conexion.Conectar()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "INSERT INTO recetas values(%s,%s,%s,%s)"
+            data = (idreceta,nombre,idproducto,cant_producto,)
+            cursor.execute(sentenciaSQL,data)
+            cursor.conexion.commit()
+            cursor.conexion.close()
+
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)            
+
+    def Insertar_Categorias(self,idcategoria,nombre,):
+        conection = conexion.Conectar()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "INSERT INTO categorias values(%s,%s)"
+            data = (idcategoria,nombre,)
+            cursor.execute(sentenciaSQL,data)
+            cursor.conexion.commit()
+            cursor.conexion.close()
+
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)       
     
+    def Insertar_Producciondiaria(self,idproduccion,idproducto,cantidad,fecha,):
+        conection = conexion.Conectar()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "INSERT INTO Produccion_diaria values(%s,%s,%s,%s)"
+            data = (idproduccion,idproducto,cantidad,fecha)
+            cursor.execute(sentenciaSQL,data)
+            cursor.conexion.commit()
+            cursor.conexion.close()
+
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)    
+
     def Listado_Productos(self):
         conection = conexion.Conectar.cursor()
         try:
@@ -58,6 +109,55 @@ class Conectar():
             resultados = cursor.fetchall()
             cursor.conexion.close()
             return resultados
-        
         except mysql.connector.Error as descripcionDelError:
             print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+    def Listado_insumos(self):
+        conection = conexion.Conectar.cursor()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "SELECT * FROM Insumos"
+            cursor.execute(sentenciaSQL)
+            resultados = cursor.fetchall()
+            cursor.conexion.close()
+            return resultados        
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+    def Listado_Recetas(self):
+        conection = conexion.Conectar.cursor()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "SELECT * FROM Recetas"
+            cursor.execute(sentenciaSQL)
+            resultados = cursor.fetchall()
+            cursor.conexion.close()
+            return resultados       
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+    def Listado_Categorias(self):
+        conection = conexion.Conectar.cursor()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "SELECT * FROM Categorias"
+            cursor.execute(sentenciaSQL)
+            resultados = cursor.fetchall()
+            cursor.conexion.close()
+            return resultados               
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+    def Listado_Producciondiaria(self):
+        conection = conexion.Conectar.cursor()
+        try:
+            cursor = conexion.Conectar.cursor()
+            sentenciaSQL = "SELECT * FROM Produccion_diaria"
+            cursor.execute(sentenciaSQL)
+            resultados = cursor.fetchall()
+            cursor.conexion.close()
+            return resultados               
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar a la Base de Datos!", descripcionDelError)
+
+            
